ea5c171e769d2bef094734b303b999be5cd259be
dev-93 <cjb940103@gmail.com>
2020-02-11 00:49:51 +0900

    make Loading screen

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
 import React from 'react';
+import Loading from "./Loading"
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.blueView} />
-      <View style={styles.orangeView} />
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  blueView: {
-    flex:1,
-    backgroundColor:"blue"
-  },
-  orangeView: {
-    flex:1,
-    backgroundColor:"orange"
-  }
-});
