1016b85921569e309ccadd2ab10af0c909ded568
dev-93 <cjb940103@gmail.com>
2020-02-12 00:39:52 +0900

    Getting the Location

--- Loading.js
+++ Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Loading(){
-    return ( 
+    return (
     <View style={styles.container}>
         <Text style={styles.text}>Getting the Weather information</Text>
     </View>
