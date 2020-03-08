677ea19310bb146c6f8ce859b0f33bdc3c4395b2
dev-93 <cjb940103@gmail.com>
2020-03-08 17:07:07 +0900

    title, subtitle 부분 추가

--- Weather.js
+++ Weather.js
@@ -8,18 +8,26 @@ const weatherOptions = {
   Thunderstorm: {
     iconName: "weather-lightning",
     gradient: ["#373B44", "#4286f4"],
+    title: "천둥이 치고 있습니다",
+    subtitle: "천둥소리가 무서워요 ㅠㅠ"
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#89F7FE", "#66A6FF"],
+    title: "이슬비가 내리고 있어요",
+    subtitle: "밖에는 우산을 가지고 다니는 사람들이 보일거에요"
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#00C6FB", "#005BEA"],
+    title: "비가 내리고 있어요..",
+    subtitle: "우산을 안들고 가면 속옷이 젖을거에요"
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "눈이 내리고 있어요 !!",
+    subtitle: "눈이 오고있으니적당히 추울거에요"
   },
   Atmosphere: {
     iconName: "weather-hail",
@@ -28,22 +36,32 @@ const weatherOptions = {
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#FF7300", "#FEF253"],
+    title: "날씨가 정말 좋아요",
+    subtitle: "밖에 날씨는 정말 깨끗해요ㅎㅎ"
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#D7D2CC", "#304352"],
+    title: "구름이 조금 껴있어요",
+    subtitle: "밖에 나가기 귀찮은 날씨에요"
   },
   Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "습한 날씨에요",
+    subtitle: "몸이 찌뿌둥할 수도 있을거에요"
   },
   Dust: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "미세먼지가..!",
+    subtitle: "마스크를 챙겨야 해요!"
   },
   Haze: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "안개 낀날",
+    subtitle: "밖에 가고 싶지 않은 날씨에요."
   }
 };
 
@@ -90,7 +108,7 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   temp: {
     fontSize: 42,
