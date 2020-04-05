54b5ac4931f5f8628c14a248d6a70b49b17360c8
dev-93 <cjb940103@gmail.com>
2020-04-05 18:24:44 +0900

    Loading 스타일 수정

--- Loading.js
+++ Loading.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Loading(){
     return (
     <View style={styles.container}>
-        <Text style={styles.text}>Getting the Weather information</Text>
+        <Text style={styles.text}>날씨 정보를 불러오는 중입니다...</Text>
     </View>
     );
 }
@@ -12,13 +12,15 @@ export default function Loading(){
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent:"flex-end",
+        justifyContent:"center",
         paddingHorizontal:30,
         paddingVertical:100,
-        backgroundColor: '#FDF6AA',
+        backgroundColor: "#004680",
     },
     text: {
-        color: "#2c2c2c",
-        fontSize:30
+        color: "white",
+        fontSize:30,
+        textAlign:"center",
+        lineHeight: 50,
     }
 })
